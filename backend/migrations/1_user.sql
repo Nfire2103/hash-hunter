f12ffd7551a34c39b75a4ddea12fbb70cfb67f13
Nathan FLATTIN <nathan.flattin@epitech.eu>
2025-03-14 12:53:02 +0100

    update: user and challenge table

--- backend/migrations/1_user.sql
+++ backend/migrations/1_user.sql
@@ -1,10 +1,14 @@
-CREATE TABLE user (
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- TODO make a 1_setup.sql with extension and trigger
+-- TODO add updated_at et created_at
+CREATE TABLE "user" (
     id uuid DEFAULT uuid_generate_v1mc(),
     email TEXT NOT NULL,
     username TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
-ALTER TABLE user ADD PRIMARY KEY (id);
-ALTER TABLE user ADD UNIQUE (email);
-ALTER TABLE user ADD UNIQUE (username);
+ALTER TABLE "user" ADD PRIMARY KEY (id);
+ALTER TABLE "user" ADD UNIQUE (email);
+ALTER TABLE "user" ADD UNIQUE (username);
--- backend/migrations/2_challenge.sql
+++ backend/migrations/2_challenge.sql
@@ -1,14 +1,17 @@
-CREATE TABLE challenge(
+CREATE TYPE blockchain_type AS ENUM ('ethereum', 'solana');
+
+CREATE TABLE challenge (
     id uuid DEFAULT uuid_generate_v1mc(),
-    author_id uuid NOT NULL REFERENCES user(id) ON DELETE CASCADE,
+    author_id uuid NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     code TEXT NOT NULL,
     bytecode BYTEA NOT NULL,
     difficulty SMALLINT NOT NULL,
     solved INTEGER NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    blockchain blockchain_type NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(), -- TODO maybe use timestampz
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 ALTER TABLE challenge ADD PRIMARY KEY (id);
