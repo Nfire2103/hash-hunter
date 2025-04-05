cacee0329b3c94846061c2634aa412b2cadbd088
Nathan FLATTIN <nathan.flattin@epitech.eu>
2025-04-05 11:41:00 +0200

    feat(frontend): vite, tailwind and shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,52 @@
+import { useState } from 'react';
 import './App.css';
+import { Button } from './components/ui/button';
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground space-y-4">
+        <h1 className="text-3xl font-bold underline">Hello Vite + React!</h1>
+        <Button
+          className="font-bold py-2 px-4 rounded"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </Button>
+        <p className="text-lg">
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <p className="text-lg">
+          <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Docs
+          </a>
+        </p>
+        <p className="text-lg">
+          <a href="https://reactjs.org" target="_blank">
+            React Docs
+          </a>
+        </p>
+        <p className="text-lg">
+          <a href="https://tailwindcss.com/docs" target="_blank">
+            Tailwind CSS Docs
+          </a>
+        </p>
+        <p className="text-lg">
+          <a
+            href="https://radix-ui.com/docs/primitives/overview"
+            target="_blank"
+          >
+            Radix UI Docs
+          </a>
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </>
   );
-};
+}
 
 export default App;
