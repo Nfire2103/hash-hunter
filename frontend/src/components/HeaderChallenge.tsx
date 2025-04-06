53ad6f4a900a29a34d8e46ea5d2988d57d8277df
alexandre <alexandre.timal@gmail.com>
2025-04-06 11:30:30 +0200

    feat(frontend): Challenge page and HeaderChallenge component

---
+++ frontend/src/components/HeaderChallenge.tsx
@@ -0,0 +1,17 @@
+export const HeaderChallenge = () => {
+  return (
+    <div className="flex-grow flex items-center justify-center">
+      <div className="w-3xl flex">
+        <img
+          src="/hashHunterLogoMark.svg"
+          alt="logo"
+          className="size-16"
+        />
+        <div className="flex flex-col justify-center">
+          <h1 className="text-xl font-medium">HashHunter</h1>
+          <h2 >6 min read · 1 correction · 1 day ago</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
